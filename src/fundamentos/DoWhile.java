78057ecec3ff5c2434dac6a2b755a3f5c56e8fe7
JulioCDias <juliozulu2008@gmail.com>
2024-07-26 17:43:57 -0300

    Exemplo de Laco de repetição For, e Do While.

---
+++ src/fundamentos/DoWhile.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int valor = 0;
+        do {
+            System.out.println("Valor da variavel é: "+ valor);
+            valor++;
+        } while (valor <= 5);
+    }
+}
