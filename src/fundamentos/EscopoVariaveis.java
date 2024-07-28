b25cb93edd6b0b3080748521e6d6639ad7c55fb8
JulioCDias <juliozulu2008@gmail.com>
2024-07-28 10:54:50 -0300

    Escopo de variaveis

---
+++ src/fundamentos/EscopoVariaveis.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class EscopoVariaveis {
+    public static void main(String[] args) {
+
+        if (true){
+            double salario = 5.550;
+            System.out.println("O salario em Double é: "+salario);
+        } else {
+            int salario = 6000;
+            System.out.println("O salario em Int é: "+salario);
+        }
+    }
+}
